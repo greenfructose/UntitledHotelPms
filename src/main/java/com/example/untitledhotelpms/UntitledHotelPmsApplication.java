33381aef0113712bf081dbe3aa23454c24153449
greenfructose <justinclarkturney@gmail.com>
2022-06-01 11:06:13 -0500

    Initial commit

---
+++ src/main/java/com/example/untitledhotelpms/UntitledHotelPmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.untitledhotelpms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UntitledHotelPmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UntitledHotelPmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/untitledhotelpms/UntitledHotelPmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.untitledhotelpms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UntitledHotelPmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
